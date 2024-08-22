a57fd6cfb4ab7a49decd930c35b6eff09d4b8e9d
Maor Cohen <cohenm818@gmail.com>
2024-08-23 01:58:46 +0300

    Add files via upload

---
+++ demo2/src/main/java/com/example/demo/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1")
+public class HelloController {
+
+    @GetMapping(path = "hello")
+    public String hello() {
+        return "hello";
+    }
+}
